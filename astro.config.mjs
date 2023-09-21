33de741f48c400573321bcb5f0c25347cd79e0c6
Miguel Mendoza <miguel@zandome.com>
2023-09-21 15:22:19 -0400

    Implement Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    analytics: true
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false
