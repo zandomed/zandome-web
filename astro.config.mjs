591a2b78e3907eddcfec2fed358e3b607f85e608
Miguel Mendoza <miguel@zandome.com>
2023-09-21 15:31:08 -0400

    Revert "Implement Vercel Analytics"
    This reverts commit 33de741f48c400573321bcb5f0c25347cd79e0c6.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
-import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    analytics: true
-  }),
   integrations: [
     tailwind({
       applyBaseStyles: false
