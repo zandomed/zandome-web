7b774d37c488654004d93dbc2f4a5325b846a1d2
Miguel Mendoza <miguel@zadndome.com>
2023-09-21 09:33:00 -0400

    Remove GA4 reference constants to env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false
+    }),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push']
+      }
     })
   ]
 })
--- src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const CONSTANTS = {
-  GA_MEASUREMENT_ID: process.env.PUBLIC_GA_MEASUREMENT_ID
-}
