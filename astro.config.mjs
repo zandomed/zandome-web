ced84db04752d381b2762b5f06adba6eb5f7eaf4
Miguel Mendoza <miguel@zandome.com>
2023-12-15 12:15:46 -0400

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -13,6 +15,7 @@ export default defineConfig({
       config: {
         forward: ['dataLayer.push']
       }
-    })
+    }),
+    react()
   ]
 })
